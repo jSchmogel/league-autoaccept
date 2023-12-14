daf29dd102f6fe953affeba66c40cf4668e59bec
jSchmogel <42974878+jSchmogel@users.noreply.github.com>
2023-12-14 16:58:40 -0600

    functioning run button, stop button does not work

--- tk.py
+++ tk.py
@@ -1,6 +1,8 @@
 from tkinter import Tk, mainloop, TOP, BOTTOM    
 from tkinter.ttk import Button 
 import tkinter as tk
+import os
+import sys
 
 
 root = tk.Tk() 
@@ -10,6 +12,10 @@ tk.Tk.iconbitmap(root, default="untitled.ico")# configure icon in normal spot
 status_canvas = tk.Canvas(root)
 task = tk.Label(status_canvas)
 task.pack(anchor=tk.E, side=tk.BOTTOM)
+
+def run():
+    
+        os.system('league_autoaccept.py')
 
 
 #Title
@@ -21,7 +27,7 @@ root.geometry('250x115')
 
 
 #Run Button
-button = Button(root, text = 'Run') 
+button = Button(root, text = 'Run', command=run) 
 button.pack(side = TOP, pady = 15) 
 #Stop Button
 button = Button(root, text = 'Stop') 
