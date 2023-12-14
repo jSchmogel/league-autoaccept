39a9a60b93cdfba4c8766cf392b4b8c03011d0f7
jSchmogel <42974878+jSchmogel@users.noreply.github.com>
2023-12-14 13:31:40 -0600

    autoaccept test with party button

--- league-autoaccept.py
+++ league-autoaccept.py
@@ -1,3 +1,20 @@
 import pyautogui
 import time
 
+def autoclick():
+
+    league_button_coords = None
+    print("Button")
+
+    while league_button_coords is None:
+        league_button_coords = pyautogui.locateOnScreen("partybutton.jpg", 0.8)
+        time.sleep(0.5)
+    print("Button Found!")
+    accept_center = pyautogui.center("partybutton.jpg")
+    pyautogui.click(accept_center)
+
+    time.sleep(5)
+    exit()
+
+autoclick()
+
